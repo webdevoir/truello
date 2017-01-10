8faeb57e549dd7e7fe9326a7bc80fdcb30871196
nickcianci <n.cianci3@gmail.com>
2017-01-10 13:51:18 -0800

    add bootstrapping for current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if user && user.is_password?(password)
+    nil
   end
 
   def self.generate_session_token
