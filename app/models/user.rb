ec3ed62945155dbcddbe2a9e307e089f7917140d
nickcianci <n.cianci3@gmail.com>
2017-01-10 13:27:34 -0800

    add users and authentication

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  validates :username,
+            :password_digest,
+            :session_token,
+            presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && user.is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    save
+    nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+    nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+end
