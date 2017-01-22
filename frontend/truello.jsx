6b58e8128e36b6c43601668ebc714f0cd11cc372
nickcianci <n.cianci3@gmail.com>
2017-01-22 15:24:30 -0800

    remove store from window

--- frontend/truello.jsx
+++ frontend/truello.jsx
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  window.store = store;
 
   Modal.setAppElement(document.body);
   const root = document.getElementById('root');
